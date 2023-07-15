108d7fc6c093b73a32addec05e484a98b6641e9c
lucaso-silva <luc.osilva@gmail.com>
2023-07-14 22:12:34 -0700

    feat: button input / style: input field error color

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,54 @@
 const listInputs = document.querySelectorAll(".input");
+const button = document.querySelector(".btn")
 
 function validField(field) {
-    const id = field.id
-     
-    if(id == "day") {
-        const day = +field.value;
-        if(day < 1 || day > 31) {
-            console.log("input a valid day")
-        } else {
-            console.log("ok ", day)
-        }   
-    }
+  const id = field.id;
 
-    else if(id == "month") {
-        const month = field.value
-        if(month < 1 || month > 12) {
-            console.log("input a valid month")
-        } else {
-            console.log("ok ", month)
-        } 
+  if (id == "day") {
+    const day = field.value;
+    if (day < 1 || day > 31) {
+        field.classList.add("input-error");
+      console.log("input a valid day");
+    } else {
+        field.classList.remove("input-error")
+      console.log("ok ", day);
     }
-
-    else if(id == "year") {
-        const year = field.value
-        if(year > 2023) {
-            console.log("input a valid year")
-        } else {
-            console.log("ok ", year)
-        } 
+  } else if (id == "month") {
+    const month = field.value;
+    if (month < 1 || month > 12) {
+        field.classList.add("input-error");
+      console.log("input a valid month");
+    } else {
+        field.classList.remove("input-error")
+      console.log("ok ", month);
     }
+  } else if (id == "year") {
+    const year = field.value;
+    if (year > 2023) {
+        field.classList.add("input-error");
+      console.log("input a valid year");
+    } else {
+        field.classList.remove("input-error")
+      console.log("ok ", year);
+    }
+  }
 }
 
-listInputs.forEach((input)=> {
-    input.addEventListener("blur", ()=> {
-        validField(input);
-    });
+listInputs.forEach((input) => {
+  input.addEventListener("blur", () => {
+    validField(input);
+    console.log(input);
+  });
+
+  input.addEventListener("keydown", (e) => {
+    if (e.key == "Enter") {
+      validField(input);
+      console.log(input);
+    }
+  });
+});
+
+button.addEventListener("click", (e)=>{
+    e.preventDefault()
+    console.log(day.value, month.value, year.value)
 })
